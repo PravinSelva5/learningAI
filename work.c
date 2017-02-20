cc0a51f3928ba63f14b28c9b2b7dd9a6cf805d10
Pravin Selvarajah <PravinSelvarajah@Pravins-MacBook-Pro.local>
2017-02-20 12:28:23 -0500

    added new file

---
+++ work.c
@@ -0,0 +1,11 @@
+//
+//  work.c
+//  
+//
+//  Created by Pravin Selvarajah on 2017-02-20.
+//
+//
+
+#include "work.h"
+
+
---
+++ work.h
@@ -0,0 +1,14 @@
+//
+//  work.h
+//  
+//
+//  Created by Pravin Selvarajah on 2017-02-20.
+//
+//
+
+#ifndef work_h
+#define work_h
+
+#include <stdio.h>
+
+#endif /* work_h */
